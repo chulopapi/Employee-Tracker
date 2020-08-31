c2181633c2dc4a19960c822f1749971eaa61e147
chulopapi <marlon.guandique@gmail.com>
2020-08-30 18:39:02 -0700

    adding create tables departmento, employee and role

---
+++ db/db.js
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_data;
+CREATE DATABASE employee_data;
+USE employee_data;
+CREATE TABLE departmento(
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+CREATE TABLE role(
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_dept FOREIGN KEY (department_id) REFERENCES departmento(id) ON DELETE CASCADE
+);
+CREATE TABLE employee(
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    role_id INTEGER(11),
+    manager_id INTEGER(11),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+);
